21429beb3633e9c43a86cf869b083690d6ccbb38
lukasz.homik <homik.lukasz@gmail.com>
2017-01-03 15:39:17 +0100

    Initial commit

---
+++ src/main/java/com/javaapplets/dao/UserDao.java
@@ -0,0 +1,39 @@
+package com.javaapplets.dao;
+
+import com.javaapplets.model.User;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao {
+
+    public List<User> getAllUsers(){
+        List<User> userList = null;
+        try {
+            File file = new File("Users.dat");
+            if (!file.exists()) {
+                User user = new User(1, "Mahesh", "Teacher");
+                userList = new ArrayList<User>();
+                userList.add(user);
+//                saveUserList(userList);
+            }
+            else{
+                FileInputStream fis = new FileInputStream(file);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                userList = (List<User>) ois.readObject();
+                ois.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return userList;
+    }
+}
---
+++ src/main/java/com/javaapplets/service/UserService.java
@@ -0,0 +1,24 @@
+package com.javaapplets.service;
+
+import com.javaapplets.dao.UserDao;
+import com.javaapplets.model.User;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/UserService")
+public class UserService {
+
+    UserDao userDao = new UserDao();
+
+    @GET
+    @Path("/users")
+    @Produces(MediaType.APPLICATION_XML)
+    public List<User> getUsers(){
+        return userDao.getAllUsers();
+    }
+}
